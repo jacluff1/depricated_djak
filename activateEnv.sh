e532f76e4d70a16ef60782e48411abac2f2960ac
Jacob <jacluff1@asu.edu>
2019-11-11 16:30:52 -0700

    Create activateEnv.sh

---
+++ activateEnv.sh
@@ -0,0 +1,6 @@
+# deactivate any curent environment
+deactivate
+# source the virtual environment
+venvSource="$home/djakVenv/bin/activate"
+printf "activating vertial environment in $venvSource;\ntype 'deactivate' to leave virtual environment\n"
+source ./djakVenv/bin/activate
